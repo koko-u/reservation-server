5b82d7ce80ace169ed9d689424eecdb3dc95978f
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-12-13 00:05:04 +0900

    create backend project

---
+++ webpack/prod.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const baseConfig = require('./base.config')
+const { merge } = require('webpack-merge')
+
+const config = merge(baseConfig, {
+  mode: 'production',
+})
+
+module.exports = config
